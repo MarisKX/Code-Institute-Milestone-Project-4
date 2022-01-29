0b14e4456e351d268f23d10f080bbd70834cdc3d
MarisKX <maris.kurpnieks@gmail.com>
2022-01-29 17:11:31 +0000

    Initial commit

---
+++ wheelshop/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for wheelshop project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wheelshop.settings')
+
+application = get_wsgi_application()
