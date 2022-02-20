1c60d5ec1e0f3f2e7d3dbcbc04d798cf6ba147db
MarisKX <maris.kurpnieks@gmail.com>
2022-02-20 18:32:12 +0000

    Added Stripe payment basic setup

--- checkout/views.py
+++ checkout/views.py
@@ -1,21 +1,43 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from cart.contexts import cart_content
+
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('cart', {})
     if not bag:
         messages.error(request, "There's nothing in your shopping cart at this moment")
         return redirect(reverse('products'))
 
+    current_cart = cart_content(request)
+    total = current_cart['total']
+
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51KHU9II4JiRbfrjT9Y46o0Tw6SbcLz6wYUCSxZsWxwIh5ur78EhPk7uDxWKmIx60XM3gBueLgEtJwolcAgOAjTbo00KrdJpRkv',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
