d19354a4bd621c114d478cd74890f0a889a405dd
MarisKX <maris.kurpnieks@gmail.com>
2022-02-27 01:43:26 +0000

    Finished product management options - add, update and delete

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:ean_code>/', views.product_details, name='product_details'),
     path('add_product/', views.add_product, name='add_product'),
     path('update_product/<int:ean_code>/', views.update_product, name='update_product'),
+    path('delete_product/<int:ean_code>/', views.delete_product, name='delete_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -175,9 +175,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_details', args=[product.ean_code]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -215,3 +215,12 @@ def update_product(request, ean_code):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, ean_code):
+    """ Edit a product in the store """
+    product = get_object_or_404(Product, ean_code=ean_code)
+    product.delete()
+    messages.success(request, 'Product deleted succcessfully!')
+    return redirect(reverse('products'))
+
