18bcebc114a613dd5d5c60a57e9a76afcd7aa35d
MarisKX <maris.kurpnieks@gmail.com>
2022-02-27 02:51:45 +0000

    Removed env variables from settings

--- wheelshop/settings.py
+++ wheelshop/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jb21$g8uz1yiqotf)#c5i$xzml50f^uve^2!i46#%ty__tas(g'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['code-institute-milestone-p4.herokuapp.com', 'localhost']
 
